842f6b62571dc755110e0a89e2b9807cc0399de9
vanvac <21ThangeVa@olympustrust.co.uk>
2024-11-25 18:54:49 +0000

    i want to nuke this code

--- snakeonplane.py
+++ snakeonplane.py
@@ -1,13 +1,4 @@
-garden = '''╔════════╗
-║.;~┌ ┐¸.║
-║.┌.:┐;:.║
-║ '~~¸::;║
-║.¸.,; ¸`║
-║~└~~┘ : ║
-║:¸¸...`.║
-║` '└:┘' ║
-║¸,.;':~~║
-╚════════╝'''
+garden = ''''''
 
 def splitIntoMultidimList(WeirdString):
   MultidimList = []
@@ -18,8 +9,8 @@ def splitIntoMultidimList(WeirdString):
 
 def findBox(multiDimList):
   boxIndexList = []
-  for x,  layer in enumerate(multiDimList):
-    for y, char in enumerate(layer):
+  for y,  layer in enumerate(multiDimList):
+    for x, char in enumerate(layer):
       if char == '┌':
         boxIndexList.append([x, y, '┌'])
       elif char == '┐':
@@ -31,22 +22,59 @@ def findBox(multiDimList):
   return boxIndexList
 
 
-def findBothLenghts(boxIndexList, direction):
-  startAt = '┌'
-  if direction == 'x':
-    cordIndex = 0
-    endAt = '┐'
-  elif direction == 'y':
-    cordIndex = 1
-    endAt = '└'
-  for corner in boxIndexList:
-    if corner[2] != startAt:
-      continue
-    
-    
-    
-  
+def findDiffAndCheckIfChange(sholdChange, notChange, shouldChangeI, notChangeI, compare):
+  if notChange != compare[notChangeI]:
+    return False, 0
+  else:
+    return True, abs(sholdChange - compare[shouldChangeI])+1
   
 
-mul = splitIntoMultidimList(garden)
-print(findBox(mul))
+def findBothLenghts(boxIndexList):
+  startAt = '┌'
+  coordIndex = 0
+  XYLenght = []
+  while coordIndex < 2:
+    if coordIndex == 0: 
+      endAt = '┐' 
+      shouldChangeIndex = 0
+      notChangeIndex = 1
+    else: 
+      endAt = '└'
+      shouldChangeIndex = 1
+      notChangeIndex = 0
+    randomFcvkingSRLatch = False
+    for coord in boxIndexList:
+      if coord[2] == startAt and not randomFcvkingSRLatch:
+        shouldChange = coord[shouldChangeIndex]
+        notChange = coord[notChangeIndex]
+        randomFcvkingSRLatch = True
+        
+      if coord[2] == endAt and randomFcvkingSRLatch:
+        isDiff, diff = findDiffAndCheckIfChange(shouldChange, notChange, shouldChangeIndex, notChangeIndex, coord)
+        if isDiff:
+          XYLenght.append(diff)
+          break
+
+    if coordIndex == 1:
+      for starCor in boxIndexList:
+        if starCor[2] == startAt:
+          boxIndexList.remove(starCor)
+          break
+    coordIndex += 1
+  return XYLenght, boxIndexList
+
+def calcArea(XYLenght):
+  return XYLenght[0] * XYLenght[1]
+
+
+cornersList = findBox(splitIntoMultidimList(garden))
+XYLenght = None
+addArea = []
+while XYLenght != []:
+  XYLenght, cornersList = findBothLenghts(cornersList)
+  try:
+    addArea.append(calcArea(XYLenght))
+  except:
+    break
+
+print(addArea)
