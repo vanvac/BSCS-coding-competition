88905b6f546150565b704d31eeaf20b440a5c1af
vanvac <21ThangeVa@olympustrust.co.uk>
2024-11-25 19:40:56 +0000

    im thinking

--- snakeonplane.py
+++ snakeonplane.py
@@ -9,28 +9,62 @@ garden = '''╔════════╗
 ║¸,.;':~~║
 ╚════════╝'''
 
-def splitIntoMultidimList(WeirdString):
+def cleanStr(messStr):
+  cleanStr = ''
+  for index, letter in enumerate(messStr):
+    if letter not in '┌┐└┘.\n':
+      cleanStr += '.'
+    else:
+      cleanStr += letter
+  return(cleanStr)
+
+garden = cleanStr(garden)
+
+def splitIntoMultidimList(notMessyString):
   MultidimList = []
-  for line in WeirdString.split('\n'):
+  for line in notMessyString.split('\n'):
     MultidimList.append(list(line))
   return MultidimList
 
 
-def findBox(multiDimList):
-  boxIndexList = []
+def findCorner(multiDimList):
+  cornerIndexList = []
   for x,  layer in enumerate(multiDimList):
     for y, char in enumerate(layer):
-      if char == '┌':
-        boxIndexList.append([x, y, '┌'])
-      elif char == '┐':
-        boxIndexList.append([x, y, '┐'])
-      elif char == '└':
-        boxIndexList.append([x, y, '└'])
-      elif char == '┘':
-        boxIndexList.append([x, y, '┘'])
-  return boxIndexList
+      if char != '.':
+        cornerIndexList.append([x,y,char])
+  return cornerIndexList
 
-def findSnakeArea(multiDimList):
+def makeOverlapCountArray(multiDimList):
+  overlapCountArray = []
+  for line in multiDimList:
+    newLine = []
+    for char in line:
+      newLine.append(0)
+    overlapCountArray.append(newLine)
+  return overlapCountArray
+
+def findHorizontalCorner(cornerIndexList):
   pass
 
-print(findBox(splitIntoMultidimList(garden)))
+def findVerticalCorner(cornerIndexList):
+  pass
+
+def findOppositeCorner(cornerIndexList):
+  pass
+
+def findBoxFromCorners(cornerIndexList):
+  boxList = []
+
+  
+
+def findSnakeArea(multiDimList):
+  overlapCountArray = makeOverlapCountArray(multiDimList)
+  cornerIndexList = findCorner(multiDimList)
+  boxes = []
+  
+
+
+
+
+print(findCorner(splitIntoMultidimList(garden)))
